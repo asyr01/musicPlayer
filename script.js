82b8d8dbc5849e56e034227c8b8b41e7493872ea
example <alisayar0107@gmail.com>
2021-01-29 15:53:25 +0300

    pause and play functionalities added

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Check if playing
+let isPlaying = false;
+
+// Play a song
+function playSong(){
+   isPlaying = true;
+   playBtn.classList.replace('fa-play', 'fa-pause');
+   music.play();
+}
+
+// Pause a song
+function pauseSong(){
+   isPlaying = false;
+   playBtn.classList.replace('fa-pause', 'fa-play');
+   music.pause();
+}
+
+
+// Event Listeners
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
