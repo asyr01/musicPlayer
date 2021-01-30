3a6ea9a22a296c5ef3b831e4eec4e6bd85f05835
example <alisayar0107@gmail.com>
2021-01-30 17:42:52 +0300

    constants for changing song added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+
 
 // Check if playing
 let isPlaying = false;
@@ -10,6 +15,7 @@ let isPlaying = false;
 function playSong(){
    isPlaying = true;
    playBtn.classList.replace('fa-play', 'fa-pause');
+   playBtn.setAttribute('title', 'Pause the song');
    music.play();
 }
 
@@ -17,6 +23,7 @@ function playSong(){
 function pauseSong(){
    isPlaying = false;
    playBtn.classList.replace('fa-pause', 'fa-play');
+   playBtn.setAttribute('title', 'Play the song');
    music.pause();
 }
 
