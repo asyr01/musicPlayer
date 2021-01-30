78bfb069a9f3e8724969f42eb57cb1d0cccb1c6a
example <alisayar0107@gmail.com>
2021-01-30 18:05:43 +0300

    prev and next functionality added with dynamically changing last to first song ande reverse of it

--- script.js
+++ script.js
@@ -58,8 +58,32 @@ function loadSong(song) {
    image.src = `img/${song.name}.jpg`;
 }
 
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+   songIndex--;
+   if(songIndex < 0 ){
+      songIndex= songs.length - 1;
+   }
+   loadSong(songs[songIndex]);
+   playSong();
+}
+
+// Next Song
+function nextSong() {
+   songIndex++;
+   if(songIndex > songs.length -1 ){
+      songIndex= 0;
+   }
+   loadSong(songs[songIndex]);
+   playSong();
+}
+
+
 // On Load - Select First song
-loadSong(songs[3]);
+loadSong(songs[songIndex]);
 
 
 // Event Listeners
