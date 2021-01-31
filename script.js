4159d479201cf4eb01cae2ea23d53f1344f0e147
example <alisayar0107@gmail.com>
2021-01-31 16:06:11 +0300

    now currentTime is changing and duration is there. Last thing is clicking and jumping to different part of the song

--- script.js
+++ script.js
@@ -98,6 +98,22 @@ function updateProgressBar(e){
        const progressPercent = ( currentTime / duration) * 100;
        progress.style.width = `${progressPercent}%`;
        // Calculate display for duration
+       const durationMinutes = Math.floor(duration / 60);
+       let durationSeconds = Math.floor(duration % 60);
+       if(durationSeconds < 10) {
+          durationSeconds = `0${durationSeconds}`
+       }
+       // Delay switching duration Element to avoid NaN
+       if(durationSeconds && durationMinutes){
+         durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+       }
+      // Calculate display for duration
+      const currentMinutes = Math.floor(currentTime / 60);
+      let currentSeconds = Math.floor(currentTime % 60);
+      if(currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`
+      }
+      currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
    }
 }
 
